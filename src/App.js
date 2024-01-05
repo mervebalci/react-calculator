dfd525dd53da2b49749e54b16edeea815c2396e2
Merve Balci <mervebalci090@gmail.com>
2024-01-05 19:56:30 -0400

    Creating the ButtonBox component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Wrapper from "./components/Wrapper";
 import Screen from "./components/Screen";
+import ButtonBox from "./components/ButtonBox";
 
 export default function App() {
   return (
     <div>
       <Wrapper>
         <Screen />
+        <ButtonBox>
+          
+        </ButtonBox>
       </Wrapper>
     </div>
   );
